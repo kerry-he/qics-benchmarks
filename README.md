03456bf76e92c8f95843eb6be0436ea9c741a2f6
kerry-he <he.kerry.k@gmail.com>
2024-10-08 15:13:27 +1100

    Added benchmark library problems

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# qics-benchmarks
-Set of benchmark problems for the QICS solver
+# Benchmark Library for QICS
+
+This repository includes benchmark libraries for semidefinite programs, 
+quantum relative entropy programs, and conic programs involving epigraphs
+of noncommutative perspective functions, which were used to evaluate the
+performance of QICS.
+
+To run QICS against these problems, you should first install QICS from pip
+by calling
+
+```bash
+pip install qics
+```
+
+then clone this repository
+
+```bash
+git clone https://github.com/kerry-he/qics-benchmarks.git
+```
+
+You can then use the ``run_benchmarks.py`` script to run QICS against all
+problems in a given folder. For example, the following lines will run QICS
+against the library of quantum relative entropy programs.
+
+```bash
+cd qics-benchmarks
+python run_benchmarks.py -d data/qreps/qics
+```
+
+This outputs a CSV file named ``qics_benchmarks_*.csv`` into the working
+directory which contains a summary of the solver outputs from QICS.
---
+++ run_benchmarks.py
@@ -0,0 +1,47 @@
+import argparse
+import csv
+import os
+import qics
+
+
+parser = argparse.ArgumentParser(description="Runs benchmarks for QICS.")
+
+parser.add_argument("-d", "--directory", type=str, required=True,
+                    help="input directory to read problems from")
+parser.add_argument("-o", "--output", type=str,
+                    help="name of file to output logs to")
+parser.add_argument("--lowaccuracy", action="store_true",
+                    help="solve problems to low accuracy, i.e., tol=1e-5 "
+                    "(default: tol=1e-8)")
+
+args = parser.parse_args()
+
+folder = args.directory.strip()
+fin_names = os.listdir(folder)
+
+if args.output is None:
+    import time
+    time_str = time.strftime("%Y-%m-%d_%H-%M-%S")
+    fout_name = "qics_benchmarks_" + time_str + ".csv"
+
+gap_tol = 1e-5 if args.lowaccuracy else 1e-8
+gap_feas = 1e-5 if args.lowaccuracy else 1e-8
+
+with open(fout_name, "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["problem", "solver", "status", "optval", "time",
+                     "iter", "gap", "pfeas", "dfeas"])
+
+for fname in fin_names:
+    # Read and solve a conic program
+    model = qics.io.read_file(folder + fname)
+    solver = qics.Solver(model, 
+                         max_iter=500, gap_tol=gap_tol, gap_feas=gap_feas)
+    info = solver.solve()
+
+    # Write solver summary to file
+    with open(fout_name, "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([fname, "ours", info["sol_status"],  info["p_obj"],
+                         info["solve_time"], info["num_iter"], 
+                         info["opt_gap"], info["p_feas"], info["d_feas"]])
